577abcf25e61c1c8457140fd7d0f533e7cd27e00
Akshat kumar <theyashgreat@gmail.com>
2023-07-03 17:55:19 +0530

    updated

---
+++ src/pages/Auth/Login.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import Layout from "./../../components/Layout/Layout";
+import axios from "axios";
+import { useNavigate, useLocation } from "react-router-dom";
+import toast from "react-hot-toast";
+import "../../styles/AuthStyles.css";
+import { useAuth } from "../../context/auth";
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [auth, setAuth] = useAuth();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+ const path='/api/v1/auth/login';
+ const API=(process.env.REACT_APP_API_KEY)+path;
+ console.log(API);
+  // form function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      var calldata = {
+        method: 'post',
+        url: API,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: JSON.stringify({
+          "email":email,
+          "password":password,
+        }),
+      };
+      const res = await axios.post('/api/v1/auth/login',{
+        email,
+        password
+      });
+      console.log("result for login ",res)
+      if (res && res.data.success) {
+        toast.success(res.data && res.data.message);
+        setAuth({
+          ...auth,
+          user: res.data.user,
+          token: res.data.token,
+        });
+        localStorage.setItem("auth", JSON.stringify(res.data));
+        navigate(location.state || "/");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+  return (
+    <Layout title="Register - Ecommer App">
+      <div className="form-container " style={{ minHeight: "90vh" }}>
+        <form onSubmit={handleSubmit}>
+          <h4 className="title">LOGIN FORM</h4>
+
+          <div className="mb-3">
+            <input
+              type="email"
+              autoFocus
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Email "
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter Your Password"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <button
+              type="button"
+              className="btn forgot-btn"
+              onClick={() => {
+                navigate("/forgot-password");
+              }}
+            >
+              Forgot Password
+            </button>
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            LOGIN
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default Login;
